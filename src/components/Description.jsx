662db39e7f0fe945747f4c6096611a85b49fd661
gcoayla <gcoayla.28@gmail.com>
2021-04-08 14:55:26 -0500

    Description component finished, updated styles

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,13 +1,65 @@
 import React from 'react'
 import {IdContext} from '../context/IdProvider'
+import { useQuery, gql } from '@apollo/client';
+import Loading from './Loading';
+import Error from './Error'
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 const Description = () => {
 
     const {idSelected} = React.useContext(IdContext)
+
+    let personQuery = gql`
+        query{
+            person(id : "${idSelected}") {
+            eyeColor
+            hairColor
+            skinColor
+            birthYear
+            starshipConnection{
+                starships {
+                name
+                }
+            }
+            }
+        }
+    `;
+
+    const { loading, error, data } = useQuery(personQuery);
+    
+    if(loading) return <Loading/>
+    if(error) return <Error/>
     
     return (
         <div className="description">
-            <h2>{idSelected}</h2>
+            <div className="wrap-description">
+                <h2 className="box-sub-title">General Information</h2>
+                <div className="box-information">
+                    <h2 className="h2-low-emphasis box-left">Eye Color</h2>
+                    <h2 className="box-right">{capitalizeFirstLetter(data.person.eyeColor)}</h2>
+                </div>
+                <div className="box-information">
+                    <h2 className="h2-low-emphasis box-left">Hair Color</h2>
+                    <h2 className="box-right">{capitalizeFirstLetter(data.person.hairColor)}</h2>
+                </div>
+                <div className="box-information">
+                    <h2 className="h2-low-emphasis box-left">Skin Color</h2>
+                    <h2 className="box-right">{capitalizeFirstLetter(data.person.skinColor)}</h2>
+                </div>
+                <div className="box-information">
+                    <h2 className="h2-low-emphasis box-left">Birth Year</h2>
+                    <h2 className="box-right">{capitalizeFirstLetter(data.person.birthYear)}</h2>
+                </div>
+                <h2 className="box-sub-title">Vehicles</h2>
+                {data.person.starshipConnection.starships.map(item =>
+                    <div className="box-information">
+                        <h2 className="h2-low-emphasis box-left">{item.name}</h2>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
